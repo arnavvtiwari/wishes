f07c162aaeb19604b78a22090b34afb77a5645c8
Arnav Tiwari <111953200+arnavvtiwari@users.noreply.github.com>
2025-04-25 00:03:02 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
-import Navigation from '../Navigation/Navigation'
+import Navigation from '../navigation/Navigation'
 
 const About = () => {
   const [wishes, setWishes] = useState([
