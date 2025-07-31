80ea7e6ce53264c5808fe320e128ee98ecb08449
Arnav Tiwari <111953200+arnavvtiwari@users.noreply.github.com>
2025-07-31 16:09:22 +0000

    31-Jul-2025

--- src/components/app/AppPages.jsx
+++ src/components/app/AppPages.jsx
@@ -1,44 +1,44 @@
 import Navigation from '../navigation/Navigation'
 const AppPage = () => {
-    return (
+  return (
     <>
-    < Navigation/>
-    <div className="bg-white/80 backdrop-blur-sm border border-lavender-300/50 rounded-2xl p-8 shadow-xl relative overflow-hidden mt-10">
-  {/* Decorative corner elements */}
-  <div className="absolute top-0 left-0 w-16 h-16 border-t-4 border-l-4 border-lavender-300 rounded-tl-2xl"></div>
-  <div className="absolute bottom-0 right-0 w-16 h-16 border-b-4 border-r-4 border-lavender-300 rounded-br-2xl"></div>
-  
-  
-  <div className="text-center max-w-2xl mx-auto">
-    <h2 className="text-3xl text-lavender-800 font-light mb-6 tracking-wide">
-      Happy Birthdayyyyy
-    </h2>
-    
-    <div className="relative py-2 mb-8">
-      <div className="absolute left-0 top-1/2 w-1/4 h-0.5 bg-lavender-300 transform -translate-y-1/2"></div>
-      <div className="absolute right-0 top-1/2 w-1/4 h-0.5 bg-lavender-300 transform -translate-y-1/2"></div>
-      <div className="text-lavender-500 text-4xl mx-auto w-max">❦</div>
-    </div>
-    
-    <div className="bg-lavender-50/50 p-6 rounded-xl mb-8 border border-lavender-200">
-      <p className="text-xl text-lavender-700 leading-relaxed font-serif italic">
-        "Happy Birthday Pallavi 🎉❤️.... can't believe ek saal itni jaldi nikal gya it was the best time of my life 💕....ik kaafi ups and downs aye pichle ek saal me tumhari life me and humare beech bhi but we are together finally 🥹....mai kitni bhi tareef krlu tumhari sb kam hi hai tum kitni special ho and kitna accha insaan ho ye mai jitni baar bolu wo kam hi hai🙂‍↕️.....maine to saari hopes hi chodh di thi ki I'll ever find someone whom I can trust and jisse mai saari baatein bata pau apni pr then I saw your loving side.... initially humari itni baatein hoti nhi thi ki I could see that side of you....but once I got to know you there was no going back.... cheezein bohot jaldi jaldi badal gyi humare beech aur sb accha hi hua and aage bhi accha hi hoga i hope🥹🐥. Pata hai mai kitna bhi pareshan rhu aur kisi se baat krne ka mann na ho mera phir bhi all I want to hear is your voice....pichle ek saal me I've become addicted to your voice....roz hi koi na koi vn sun rha hota hu tumhara. I really appreciate your presence cutie...bs tumhara hona hi mere liye bohot badi assurance hai...mere jaisa introvert insaan ek naye sheher me akele itna comfortablely reh rha hai just because of you....tumse baat krna mujhe aisa feel karata hai ki I have someone, someone who cares about me....itne logo me koi hai jisse farak padta hai ki aaj maine kuch khaya ya nhi jisko janna hota hai mai din ke har minute kya kr rha hota hu....ik ab hum relationship me hain and this is what couples do pr it's always special with you....tumhara care aur affection mujhe motivate karta hai ki I need to be worthy of you. Pata hai jb bhi hum mile hain mujhe kabhi aisa nhi laga ki hum itne door hain ek doosre se it was always like ki roz to baat krte hain hum....pata nhi maine kabhi bataya bhi ya nhi pr jb hum pichle saal mile the uss event ne mera perspective hi badal diya ki koi itna bhi bharosa kr sakta hai mujhpr....mai kitne dino tk to comprehend hi nhi kr paya ki ye hua kya tumne kaise decide kr liya ki hum room share krenge. Tumhare uss decision ne mera perspective hi badal diya tha ki koi to hai jo itna bharosa kr rha hai mujh pr....uske baad itni saari choti choti cheezien hoti gyi ki mujhe kb pyaar ho gya pata hi nhi laga....tum kitni baar call pe bolti ho ki tum samajh hi nhi payi ki mai hints de rha tha pr aisa nhi tha bhai mai koi hints nhi de rha tha wo sb bs mai kar rha hota tha ki aisa karta hu to shayad tumhe accha lage bs kuch na kuch krta rehta tha jisse tumhe accha feel ho....aur ek cheez ye bhi thi ki jb bhi mai reply nhi krta tha ya kaafi der online nhi aata tha to tum pareshan hoti thi usse mujhe feel hota tha ki tumhe sach me farak padta hai mere hone na hone se aur wo ek responsibility jaisi thi mere liye ki tumhe time se reply kru saari updates deta rhu aur baatein sunu tumhari....uss time tk mai sochta bhi nhi tha ki hum kbhi yaha tk aa bhi jayenge mujhe bs yahi lagta tha ki tum khush rho mere saath mere liye wo bohot badi cheez hai.....tumhare pichle birthday ki itni excitement thi mujhe ki birthday pe paragraph likh ke bhejunga tumko....kitne dino tk alag alag paragraph likhe the maine ki ye accha nhi lag rha etc etc....pr finally tumhe wo accha laga tha to mai bohot khush tha uss din aur tumne dora ki dp bhi lagai thi🥹.....tumhara naam ab change kr diya hai maine pr abhi bhi log tumhe dora naam se hi jante hain....mujhe ye accha to lagta hai pr ig ab har kisi ko Pallavi hi pata hona chahiye isiliye maine naam change kiya aur photo bhi laga di ki incase koi contact dekh bhi le to sbko naam aur photo to pata hi ho....mere saare dost yahi puchte hain dora kaisi hai pr I feel dora name maine rakha tha aur wo bohot hi personal cheez hai mere liye itne dino se mera mann nhi maan rha tha change krne ka pr mai nhi chahta koi bhi iss impression me rhe ki naam chupane ke liye dora save kiya hai maine.... I'm very proud of you cutie aur tum achievement ho meri life ki and I want everyone to know your name. Ab to mere friend circle me itni common knowledge ho gyi hai ki mai relationship me hu log randomly poke kr ke chale jaate hain aur mai blush hi krta rehta hu🥹....pr everyone knows kitna serious hu mai tumhare liye... isliye nhi ki maine kisi ko bataya hai pr isliye ki har kisi ko dikhta hai ki kitna invested hu mai tumme....kahi bhi jaata hu tumse hi baat kr rha hota hu koi call karta hai aur agar phone busy aya to sb khud hi samajh jaate hain tumse hi baat kr rha hu. Ik cheeze ab pehle jaisi nhi hain mai time nhi de pata tumhe aur utna emotional support bhi nhi de rha...hain kuch reasons bhai mujhe bhi nhi samajh aata kyu itna exhausted hu ig nayi jagah hai naya environment hai isliye pr I promise ye temporary phase hai and chala jayega thode time ke saath....bs bharosa rakhna mujh pr mai tumhare saath kuch bhi galat nhi kr rha bs thoda time maang rha hu iss nyi lifestyle me adjust hone ke liye....ik mai bohot expressive nhi hu aur meri baaton se nhi lagta ki kitna dependent hu tumpr lekin agar ek din bhi baat na kru tumse to dimaag me alag alag khayal aane lagte hain mere....jbse banglore aya hu tumne bohot support kiya mera...roz mere liye time nikala roz baat ki mera rude behaviour tolerate kiya you did everything....aur mai bohot thankful hu ki tumne samajhne ki koshish ki meri situation aur itna time diya mujhe.... I really appreciate you babyy you are the best human being I've met and I'm glad ki you are mine....I LOVE YOU PALLAVI 💖. HAPPY BIRTHDAY CUTIE ❣️ Enjoy your day 💕"
-      </p>
-    </div>
-    
-    <div className="border-t border-lavender-200 pt-6 relative">
-      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-lavender-100 px-4 text-lavender-500">
-        ❦
+      < Navigation />
+      <div className="bg-white/80 backdrop-blur-sm border border-lavender-300/50 rounded-2xl p-8 shadow-xl relative overflow-hidden mt-10">
+        {/* Decorative corner elements */}
+        <div className="absolute top-0 left-0 w-16 h-16 border-t-4 border-l-4 border-lavender-300 rounded-tl-2xl"></div>
+        <div className="absolute bottom-0 right-0 w-16 h-16 border-b-4 border-r-4 border-lavender-300 rounded-br-2xl"></div>
+
+
+        <div className="text-center max-w-2xl mx-auto">
+          <h2 className="text-3xl text-lavender-800 font-light mb-6 tracking-wide">
+            Heart felt apollogies
+          </h2>
+
+          <div className="relative py-2 mb-8">
+            <div className="absolute left-0 top-1/2 w-1/4 h-0.5 bg-lavender-300 transform -translate-y-1/2"></div>
+            <div className="absolute right-0 top-1/2 w-1/4 h-0.5 bg-lavender-300 transform -translate-y-1/2"></div>
+            <div className="text-lavender-500 text-4xl mx-auto w-max">❦</div>
+          </div>
+
+          <div className="bg-lavender-50/50 p-6 rounded-xl mb-8 border border-lavender-200">
+            <p className="text-xl text-lavender-700 leading-relaxed font-serif italic">
+              "Hello babyyy🐥....I'm so sorry for what I made you feel in the past two months, ik I have not been a good partner all these days....maine aisa kuch bhi nhi kiya itne dino me ki tumhe accha lage mere saath rehne me and ik it's all my fault, I thank you for being patient all these days....I just want you to know that I love you the most and you are the most precious person in my life. I am so sorry for all the pain I caused, idk mujhe kya ho gya tha I took this relationship and you for granted. You are way too special for me and I know you deserve someone way better than me, you've been the best person to be with....you are the ideal partner but I have been no where near that. But I promise that I will change myself and make you feel the same like I used to before, ik tumhe aisa lagta hai ki maine jo bhi promises kiye the sb jhoot the and mai ab badal gya hu pr aisa kuch bhi nhi hai....you will always be my top priority, there nothing more important for me....your happiness matters the most to me and I really mean it. Ye sb jo bhi hua ye sb bs ek phase tha meri life ka jisme mujhe nhi samajh aya ki mai kya karu aur mai apni cheezo me zada hi busy ho gya tha....itna ki maine kuch kiya hi nhi tumhare liye itne dino me aut ab mujhe samajh aata ki what it made you feel....it was all my fault and I am really really sorry you had to go through all that and I assure you that all this will change because you and I are permanent and all these problems will come and go.....Ik jo bhi hua wo sb meri galti thi and mujhe dhyan dena chahiye tha iss relationship me....I'm really sorry pumpkin I hope you forgive me and things become like they were. I really love you pumpkin❤️"
+            </p>
+          </div>
+
+          <div className="border-t border-lavender-200 pt-6 relative">
+            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-lavender-100 px-4 text-lavender-500">
+              ❦
+            </div>
+            <p className="text-lg text-lavender-600 italic">
+              "The greatest apology is changed behavior."
+            </p>
+            <p className="text-lavender-500 mt-1">- Pineapple</p>
+          </div>
+        </div>
       </div>
-      <p className="text-lg text-lavender-600 italic">
-        "The greatest apology is changed behavior."
-      </p>
-      <p className="text-lavender-500 mt-1">- Pineapple</p>
-    </div>
-  </div>
-</div>
-      </>
-    )
-  }
-  
-  export default AppPage
+    </>
+  )
+}
+
+export default AppPage
